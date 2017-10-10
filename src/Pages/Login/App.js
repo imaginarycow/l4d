e5a34011ae9c8a74285c7214678decdba09bcdcf
Ramiro Beltran <beltran.ramiro@hotmail.com>
2017-10-09 19:54:37 -0500

    login

--- src/Pages/Login/App.js
+++ src/Pages/Login/App.js
@@ -1,45 +1,99 @@
 import React, { Component } from 'react';
+import { validateEmail, validatePassword } from '../../utils/validations';
 import './css/login.css';
 
+var eSuccess = false;
+var pSuccess = false;
 
 class App extends Component {
 
   constructor() {
     super();
 
-    this.state = {needsAccount: false}
+    this.state = {
+      needsAccount: false,
+      email: '',
+      pass: '',
+      attempts: 0
+    }
 
     this.login = this.login.bind(this);
-    this.createAccountView = this.createAccountView.bind(this);
+    this.signUp = this.signUp.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.toggleAccountView = this.toggleAccountView.bind(this);
+  }
+
+  onChange(e) {
+    e.preventDefault();
+    if (e.target.name === 'email') {
+      this.setState({email: e.target.value});
+    }
+    if (e.target.name === 'pass') {
+      this.setState({pass: e.target.value});
+    }
   }
 
   login(e) {
     e.preventDefault();
+    //attempt user login with state.email & state.pass
+    //after 3 login attempts, deny further attempts
     console.log('login attempted');
+
+  }
+  signUp(e) {
+    e.preventDefault();
+    //attempt create user with state.email & state.pass
+    //validate email address, verify unique user
+    console.log('signup attempted');
+    eSuccess = validateEmail(this.state.email);
+    pSuccess = validatePassword(this.state.pass);
+    console.log('validateions: ' + eSuccess + ' ' +pSuccess);
   }
 
-  createAccountView(e) {
+  toggleAccountView(e) {
     e.preventDefault();
-    console.log('switch to createAccount view');
+    console.log('toggle account view');
+    if (this.state.needsAccount) {
+      this.setState({needsAccount: false, pass: ''});
+    }
+    else {
+      this.setState({needsAccount: true, pass: ''});
+    }
+
   }
 
   render() {
-
-      return (
-        <div id="loginContainer">
-          <form id="form" onSubmit={this.login}>
-            <h3 id="loginLabel">Login</h3>
-            <label id="elabel">Email</label>
-            <input id="email" type="text" />
-            <label id="plabel">Password</label>
-            <input id="pass" type="text" />
-            <input id="submit" type="submit" value="Sign In" />
-            <label id="label3">Don't have a Left4Dev acount?</label>
-            <button id="create" onClick={this.createAccountView}>Creat a free account</button>
-          </form>
-        </div>
-
-      );
+      if (!this.state.needsAccount) {
+        return (
+          <div id="loginContainer">
+            <form id="form" onSubmit={this.login}>
+              <h3 id="loginLabel">Login</h3>
+              <label id="elabel">Email</label>
+              <input id="email" type="text" value={this.state.email} onChange={this.onChange} name="email" />
+              <label id="plabel">Password</label>
+              <input id="pass" type="password" value={this.state.pass} onChange={this.onChange} name="pass" />
+              <input id="submit" type="submit" value="Sign In" />
+              <label id="label3">Don't have a Left4Dev acount?</label>
+              <button id="create" onClick={this.toggleAccountView}>Create a free account</button>
+            </form>
+          </div>
+        );
+      }
+      else {
+        return (
+          <div id="loginContainer">
+            <form id="form" onSubmit={this.signUp}>
+              <h3 id="loginLabel">Create a new account</h3>
+              <label id="elabel">Email</label>
+              <input id="email" type="text" value={this.state.email} onChange={this.onChange} name="email" />
+              <label id="plabel">Password</label>
+              <input id="pass" type="password" placeholder="must be at least 8 characters" value={this.state.pass} onChange={this.onChange} name="pass" />
+              <input id="submit" type="submit" value="Create Account" />
+              <button id="create" onClick={this.toggleAccountView}>Back to sign in</button>
+            </form>
+          </div>
+        );
+      }
   }
 }
 
---
+++ src/utils/validations.js
@@ -0,0 +1,18 @@
+export function validateEmail(email) {
+  //1. Validate good email
+
+
+  //2. Validate email not already signed up
+
+  console.log('test email validation on: ' + email);
+  return true;
+}
+
+export function validatePassword(pass) {
+  //1. Validate only good characters
+
+
+  //2. Validate correct length
+  console.log('test password validation on: ' + pass);
+  return true;
+}
